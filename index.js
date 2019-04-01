102355646055f18d8330f7c4094a44352409d656
iggfoot <ijneville19@gmail.com>
2019-04-01 19:22:35 -0400

    updated js file

--- index.js
+++ index.js
@@ -5,3 +5,35 @@
 
 /* In this exercise, the parent element that will receive
    the listener is 'shopping-list'*/ 
+
+$(function() {
+    // creates an event listener when 'delete' button is clicked; item is deleted
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+      }); 
+    /*creates an event listener when 'check' button is clicked; item toggled
+      between being crossed out and normal*/
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+      });
+    //creates an event listener when 'add an item' is clicked  
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const addItem = $('#shopping-list-entry').val(); 
+        //clears out field by setting it to empty string
+        $('#shopping-list-entry').val('');
+        $('.shopping-list').append(
+          `<li>
+            <span class="shopping-item">${addItem}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`);
+      });
+
+})
