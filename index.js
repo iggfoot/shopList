06740e7bc54b25ca088ebe027109691bb96bf7cf
iggfoot <ijneville19@gmail.com>
2019-04-01 18:12:14 -0400

    linked html to js & updated inde.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/* The purpose of the exercise to practice event delegation -
+   attaching a single event listener to a parent element that
+   will fire for all descendants whether or not they exist now
+   or in the future*/
+
+/* In this exercise, the parent element that will receive
+   the listener is 'shopping-list'*/ 
